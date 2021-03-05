5c976dd757fb16c09c9e42df39c23e9b10362b87
Rekyt <matthias.grenie@ens-lyon.fr>
2021-03-05 13:01:58 +0100

    Extract depends links from full table

--- getting_raw_data.R
+++ getting_raw_data.R
@@ -9,3 +9,19 @@ cran_pkgs = search_terms %>%
   bind_rows() %>%
   distinct() %>%
   mutate(query_date = current_date)
+
+
+pkg_df = readxl::read_xlsx("data/Table comparing taxonomic tools.xlsx",
+                           na = c("", "NA"))
+cran_pkg = pkg_df %>%
+  filter(`Should we include this package in our review?` == "include",
+         !is.na(`Release URL (CRAN / Bioconductor)`))
+  
+deps_pkgs = cran_pkg %>%
+  filter(grepl("cran", `Release URL (CRAN / Bioconductor)`)) %>%
+  select(pkg_name = `Package Name`) %>%
+  mutate(dep_df = purrr::map(pkg_name,
+                             ~.x %>%
+                               crandep::get_dep(c("Imports")) %>%
+                               tibble::enframe("dep_type", "dep_name") %>%
+                               mutate(dep_type = "Imports")))
