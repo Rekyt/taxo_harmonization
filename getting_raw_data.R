68d56b8013782158a3d70bab68f064e64285313b
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2021-03-22 18:28:48 +0100

    Building an actual directed dependency graph

--- getting_raw_data.R
+++ getting_raw_data.R
@@ -75,6 +75,41 @@ all_pkgs$resolve()
 all_pkgs$solve()
 all_pkgs$draw()
 
+all_pkgs_df = all_pkgs$get_resolution()
+
+dep_df = all_pkgs_df %>%
+  select(pkg = ref, deps) %>%
+  tidyr::unnest(deps) %>%
+  select(pkg, package, type) %>%
+  mutate(type = tolower(type)) %>%
+  distinct()
+
+pkg_info_df = bind_rows(
+  distinct(dep_df, pkg),
+  distinct(dep_df, pkg = package)
+  ) %>%
+  distinct() %>%
+  full_join(
+    pkg_df %>%
+      mutate(sub_name = case_when(
+        `Package Name` == "traitdataform" ~ "EcologicalTraitData/traitdataform",
+        `Package Name` == "taxizedb" ~ "ropensci/taxizedb",
+        !is.na(`Release URL (CRAN / Bioconductor)`) ~ `Package Name`,
+        !is.na(`Development Version`) ~ gsub("https://github.com/", "",
+                                             `Development Version`))) %>%
+      select(sub_name, 3:4) %>%
+      rename(
+        pkg = sub_name,
+        inclusion = `Should we include this package in our review?`,
+        category  = `Is this package central in taxonomic harmonization workflow?`
+      ),
+    by = c("pkg")
+  )
+
+dep_graph = dep_df %>%
+  filter(type != "enhances", type != "linkingto") %>%
+  igraph::graph_from_data_frame(vertices = pkg_info_df)
+
 # GitHub packages with GH API --------------------------------------------------
 
 gh_query = gh::gh("GET /search/repositories", q = "taxonomy+language:R")
