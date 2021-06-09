2999d8f7fcfb7e0cf1ccb60af96f12bd20ac3bb7
Alban Sagouis <alban.sagouis@idiv.de>
2021-06-09 15:53:24 +0200

    splitting build_pkg_network.R into 2 scripts
    Avoiding running the dependency building before building the network tables

--- build_network_tables.R
+++ build_network_tables.R
@@ -1,56 +1,6 @@
-# Companion script to taxonomic harmonization project
-
-# Load packages ----------------------------------------------------------------
-library("dplyr")
-
-# Load and wrangle data --------------------------------------------------------
-
-# Raw data from the Google Doc sheets exported as XLSX file
-raw_pkg_table = readxl::read_xlsx("data_raw/Table comparing taxonomic tools.xlsx",
-                                  na = c("", "NA"))
-
-# Get the list of packages that are included in the review
-included_pkg = raw_pkg_table %>%
-  filter(`Should we include this package in our review?` == "include")
-
-# List of included packages that are on CRAN
-cran_pkg = included_pkg %>%
-  filter(!is.na(`Release URL (CRAN / Bioconductor)`))
-
-
-# Dependency network with {pkgdepends} -----------------------------------------
-
-# Create column 'network_name' to extract deps with pkgdepends
-included_pkg = included_pkg %>%
-  mutate(network_name = case_when(
-    # Change taxizedb to be compatible with ropensci/taxview dependency
-    `Package Name` == "taxizedb" ~ "ropensci/taxizedb",
-    # If package on CRAN directly use its name
-    !is.na(`Release URL (CRAN / Bioconductor)`) ~ `Package Name`,
-    # Otherwise get GitHub andle
-    !is.na(`Development Version`) ~ gsub("https://github.com/", "",
-                                         `Development Version`),
-    TRUE ~ NA_character_
-  )) %>%
-  # Additional dependencies not mentioned explicitly in packages
-  add_row(network_name = "joelnitta/jntools") %>%
-  add_row(network_name = "gustavobio/tpldata")
-
-# Retrieve dependencies for all included packages
-pkg_deps = included_pkg %>%
-  pull(network_name) %>%
-  pkgdepends::new_pkg_deps()
-
-pkg_deps$resolve()
-pkg_deps$solve()
-pkg_deps$draw()
-
-pkg_deps_df = pkg_deps$get_resolution()
-
-saveRDS(pkg_deps_df, "data_cleaned/pkg_deps_df.Rds", compress = TRUE)
-
-
 # Package igraph network -------------------------------------------------------
+pkg_deps_df <- readRDS("data_cleaned/pkg_deps_df.Rds")
+
 # Edge list data.frame
 dependencies_edge_df = pkg_deps_df %>%
   select(pkg = ref, deps) %>%
@@ -270,6 +220,14 @@ all_nodes = bind_rows(
           node_type = "package")
 
 all_nodes$title <- all_nodes$id
+all_nodes$label <- all_nodes$id
+
+all_nodes <- all_nodes %>% mutate(shape = case_when(
+  node_type == "db" ~ "diamond",
+  node_type == "package" ~ "ellipse",
+  TRUE ~ node_type
+))
+
 all_nodes <- rename(all_nodes, group = node_type)
 
 save(all_nodes, all_edges, file = "taxtool-selecter/shiny_data/full_network.Rdata")
---
+++ build_pkg_db_dependencies.R
@@ -0,0 +1,51 @@
+# Companion script to taxonomic harmonization project
+
+# Load packages ----------------------------------------------------------------
+library("dplyr")
+
+# Load and wrangle data --------------------------------------------------------
+
+# Raw data from the Google Doc sheets exported as XLSX file
+raw_pkg_table = readxl::read_xlsx("data_raw/Table comparing taxonomic tools.xlsx",
+                                  na = c("", "NA"))
+
+# Get the list of packages that are included in the review
+included_pkg = raw_pkg_table %>%
+  filter(`Should we include this package in our review?` == "include")
+
+# List of included packages that are on CRAN
+cran_pkg = included_pkg %>%
+  filter(!is.na(`Release URL (CRAN / Bioconductor)`))
+
+
+# Dependency network with {pkgdepends} -----------------------------------------
+
+# Create column 'network_name' to extract deps with pkgdepends
+included_pkg = included_pkg %>%
+  mutate(network_name = case_when(
+    # Change taxizedb to be compatible with ropensci/taxview dependency
+    `Package Name` == "taxizedb" ~ "ropensci/taxizedb",
+    # If package on CRAN directly use its name
+    !is.na(`Release URL (CRAN / Bioconductor)`) ~ `Package Name`,
+    # Otherwise get GitHub andle
+    !is.na(`Development Version`) ~ gsub("https://github.com/", "",
+                                         `Development Version`),
+    TRUE ~ NA_character_
+  )) %>%
+  # Additional dependencies not mentioned explicitly in packages
+  add_row(network_name = "joelnitta/jntools") %>%
+  add_row(network_name = "gustavobio/tpldata")
+
+# Retrieve dependencies for all included packages
+pkg_deps = included_pkg %>%
+  pull(network_name) %>%
+  pkgdepends::new_pkg_deps()
+
+pkg_deps$resolve()
+pkg_deps$solve()
+pkg_deps$draw()
+
+pkg_deps_df = pkg_deps$get_resolution()
+
+saveRDS(pkg_deps_df, "data_cleaned/pkg_deps_df.Rds", compress = TRUE)
+
