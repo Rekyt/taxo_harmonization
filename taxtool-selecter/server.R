8ae8f5d645391b9616ed25fac2849b43590f52e3
Rekyt <matthias.grenie@ens-lyon.fr>
2021-06-21 15:54:34 +0200

    Better legend and package in monospace font

--- taxtool-selecter/server.R
+++ taxtool-selecter/server.R
@@ -12,16 +12,18 @@ shinyServer(function(input, output, session) {
   )
   
   
-  # legend preparation
-  ledges <- data.frame(color = c("#998EC3", "steelblue", "#B35806"), 
-                       label = c("depends", "accesses", "populates"),
-                       arrows = c("to", "to", "to"), 
-                       font.align = "top")
+  # Additional Legend for Edges (not included by default)
+  ledges <- data.frame(
+    color = c("#998EC3", "steelblue", "#B35806"),
+    label = c("package\ndepends on", "package\naccesses",
+              "database\npopulates"),
+    arrows = c("to", "to", "to"), 
+    font.align = "middle")
   
   
   output$full_network_interactive <-  visNetwork::renderVisNetwork(
-    visNetwork(all_nodes, all_edges) %>% 
-      
+    visNetwork(all_nodes, all_edges) %>%
+      # Databases
       visGroups(groupname = "database", shape = "dot", color = list(
         background = "#F1A340",
         border = "#B35806",
@@ -30,14 +32,17 @@ shinyServer(function(input, output, session) {
           border = "#F1A340"
         ))) %>%
       
+      # Packages
       visGroups(groupname = "package", shape = "dot", color = list(
         background = "#998EC3",
         border = "#542788",
         highlight = list(
           background = "#542788",
           border = "#998EC3"
-        ))) %>%
+        )),
+        font = list(face = "Courier New")) %>%
       
+      # Interaction Options
       visInteraction(dragNodes = TRUE, 
                      dragView = TRUE, 
                      zoomView = TRUE,
@@ -48,6 +53,7 @@ shinyServer(function(input, output, session) {
                      tooltipStay = 60000L,
                      tooltipDelay = 300L) %>%
       
+      # Other options
       visOptions(
         # Add a dropdown menu in which to select the group value that we want
         # highlighted. Any column called "group"
@@ -58,6 +64,8 @@ shinyServer(function(input, output, session) {
         ),
         nodesIdSelection = TRUE
       ) %>%
+      
+      # Legend options
       visLegend(
         enabled = TRUE,
         useGroups = TRUE,
