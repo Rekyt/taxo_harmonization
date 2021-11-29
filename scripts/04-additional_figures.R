2b1743b5075bf6bc96650fa980c2b4ffb3bf9a35
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2021-11-29 16:54:03 +0100

    Add network of packages

--- scripts/04-additional_figures.R
+++ scripts/04-additional_figures.R
@@ -2,9 +2,11 @@
 # Packages ---------------------------------------------------------------------
 library("dplyr")
 library("ggplot2")
+library("ggraph")
 
 # Load data --------------------------------------------------------------------
 
+# Databases data
 database_df = readxl::read_xlsx(
   "data/data_raw/Table comparing taxonomic tools.xlsx",
   sheet = 4, na = c("", "NA")
@@ -14,6 +16,11 @@ raw_db_links_df = readxl::read_xlsx(
   "data/data_raw/Table comparing taxonomic tools.xlsx",
   sheet = 6, na = c("", "NA")
 )
+
+# Packages data
+pkg_deps_df = readRDS("data/data_cleaned/pkg_deps_df.Rds")
+pkg_tax_deps = readRDS("data/data_cleaned/taxonomy_pkg_dependencies.Rds")
+included_pkg = readRDS("data/data_cleaned/included_pkg.Rds")
 
 # Figure of Number of Databases Indexed by categories --------------------------
 
@@ -34,3 +41,34 @@ fig_number_dbs = database_df %>%
 
 
 # Relationship network between databases ---------------------------------------
+
+# see '03-draw_db_network.R'
+
+# Network of packages ----------------------------------------------------------
+
+pkg_network = igraph::graph_from_data_frame(
+  pkg_tax_deps %>%
+    mutate(source_pkg = ifelse(source_pkg == "ropensci/taxizedb", "taxizedb",
+                               source_pkg)),
+  vertices = included_pkg %>%
+    select(network_name) %>%
+    mutate(network_name = ifelse(
+      network_name == "ropensci/taxizedb", "taxizedb", network_name
+      )
+    )
+)
+
+plot_pkg_network = ggraph(pkg_network, layout = "kk") + 
+  geom_edge_link(
+    edge_width = 1, colour = "#88b4da",
+    arrow = arrow(length = unit(5, 'mm'), angle = 4, type = "closed")
+  ) + 
+  geom_node_point(size = 3, shape = 21, colour = "white", fill = "black") +
+  geom_node_text(
+    aes(label = gsub("[[:alpha:],-]+/", "", name)), family = "mono",
+    fontface = "bold", vjust = 2, size = 4, check_overlap = TRUE
+  ) +
+  theme_void()
+
+plot_pkg_network
+
