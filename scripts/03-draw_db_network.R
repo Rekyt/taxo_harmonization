a7614cc3348789189d3e6172815105f0c60035b1
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2021-12-01 17:25:01 +0100

    Better database and package network

--- scripts/03-draw_db_network.R
+++ scripts/03-draw_db_network.R
@@ -17,18 +17,26 @@ biggest_component = db_graph %>%
 
 # Plot data --------------------------------------------------------------------
 
-plot_graph_db = ggraph(biggest_component, layout = "kk") + 
+plot_graph_db = ggraph(db_graph, layout = "nicely") + 
   geom_edge_link(
-    edge_width = 1, colour = "#88b4da",
-    arrow = arrow(length = unit(5, 'mm'), angle = 4, type = "closed")
+    edge_width = 0.7, colour = "#BBCCD0",
+    arrow = arrow(length = unit(3, 'mm'), angle = 15, type = "closed")
   ) + 
-  geom_node_point(size = 3, shape = 21, colour = "white", fill = "black", stroke = 1.5) +
+  geom_node_point(aes(size = centrality_pagerank(),
+                      fill = centrality_pagerank()),
+                  shape = 21, colour = "white", stroke = 0.1) +
   geom_node_text(aes(label = name),
-                 fontface = "bold", vjust = 2, size = 4) +
+                 fontface = "bold", vjust = 2, size = 3, check_overlap = TRUE) +
+  scale_fill_viridis_b(name = "Centrality") +
+  scale_size_binned(range = c(2, 6), guide = NULL) +
   theme_void()
 
+plot_graph_db
 
 # Save plot --------------------------------------------------------------------
 
 ggsave("figures/fig3_databases_network.png", plot_graph_db, width = 700,
+       height = 400, units = "px", dpi = 300, scale = 4)
+
+ggsave("figures/fig3_databases_network.svg", plot_graph_db, width = 700,
        height = 400, units = "px", dpi = 300, scale = 4)
--- scripts/04-additional_figures.R
+++ scripts/04-additional_figures.R
@@ -27,14 +27,15 @@ included_pkg = readRDS("data/data_cleaned/included_pkg.Rds")
 fig_number_dbs = database_df %>%
   mutate(
     `Spatial Scale` = `Spatial Scale` %>%
-           factor(levels = c("Regional", "Global")),
+      factor(levels = c("Regional", "Global")),
     `Taxonomic Breadth` = `Taxonomic Breadth` %>%
       factor(levels = c("Small", "Medium", "Large"))
-    ) %>% 
+  ) %>% 
   ggplot(aes(`Taxonomic Breadth`, `Spatial Scale`)) +
   geom_count(aes(color = after_stat(n), size = after_stat(n))) +
   guides(color = 'legend') +
   scale_color_viridis_c() +
+  scale_size(range = c(1.5, 7)) +
   labs(color = "N. databases", size = "N. databases") +
   theme_bw(18) +
   theme(legend.position = "top")
@@ -52,23 +53,57 @@ pkg_network = igraph::graph_from_data_frame(
                                source_pkg)),
   vertices = included_pkg %>%
     select(network_name) %>%
-    mutate(network_name = ifelse(
-      network_name == "ropensci/taxizedb", "taxizedb", network_name
+    mutate(
+      network_name = ifelse(
+        network_name == "ropensci/taxizedb", "taxizedb", network_name
       )
     )
 )
 
-plot_pkg_network = ggraph(pkg_network, layout = "kk") + 
+access_tpl = included_pkg %>%
+  select(network_name, `Which authority?`) %>%
+  mutate(
+    network_name = ifelse(
+    network_name == "ropensci/taxizedb", "taxizedb", network_name
+  ),
+  access_tpl = grepl("TPL", `Which authority?`, fixed = TRUE)
+  ) %>%
+  filter(access_tpl) %>%
+  pull(network_name)
+
+is_ropensci = included_pkg %>%
+  select(dev_version = `Development Version`, network_name) %>%
+  mutate(
+    network_name = ifelse(
+      network_name == "ropensci/taxizedb", "taxizedb", network_name
+    ),
+    is_ropensci = grepl("ropensci", dev_version, fixed = TRUE)
+  ) %>%
+  filter(is_ropensci) %>%
+  pull(network_name)
+
+plot_pkg_network = ggraph(pkg_network, layout = "nicely") + 
   geom_edge_link(
-    edge_width = 1, colour = "#88b4da",
-    arrow = arrow(length = unit(5, 'mm'), angle = 4, type = "closed")
+    edge_width = 0.7, colour = "#BBCCD0",
+    arrow = arrow(length = unit(3, 'mm'), angle = 15, type = "closed")
   ) + 
-  geom_node_point(size = 3, shape = 21, colour = "white", fill = "black") +
+  geom_node_point(aes(fill = name %in% access_tpl,
+                      colour = name %in% is_ropensci),
+                  size = 3.2, shape = 21, stroke = 1) +
   geom_node_text(
     aes(label = gsub("[[:alpha:],-]+/", "", name)), family = "mono",
     fontface = "bold", vjust = 2, size = 4, check_overlap = TRUE
   ) +
-  theme_void()
+  scale_colour_manual(values = c(`TRUE` = "black", `FALSE` = NULL)) +
+  labs(fill = "Access The Plant List?",
+       colour = "Is rOpenSci package?") +
+  theme_void() +
+  theme(legend.position = "top")
 
 plot_pkg_network
 
+ggsave("figures/figX_packages_network.svg", plot_pkg_network, width = 700,
+       height = 400, units = "px", dpi = 300, scale = 4)
+
+ggsave("figures/figX_packages_network.png", plot_pkg_network, width = 700,
+       height = 400, units = "px", dpi = 300, scale = 4)
